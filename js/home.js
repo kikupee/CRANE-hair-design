14768c17a02168c0482357e592cfa35b4dc2dadf
kikupee <kinaco157@gmail.jp>
2021-02-09 01:13:29 +0900

    slick完了　news完了

--- js/home.js
+++ js/home.js
@@ -5,7 +5,7 @@ $('.style_list').slick({
   arrows: false,
   infinite: true,
   speed: 300,
-  slidesToShow: 4,
+  slidesToShow: 5,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 0,
@@ -22,14 +22,14 @@ $('.style_list').slick({
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 4,
         slidesToScroll: 1,
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1
       }
     }
